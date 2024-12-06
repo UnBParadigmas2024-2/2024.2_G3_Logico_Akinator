5774ae34bf0dc77d534623a43c75ba1cca19ed76
PabloChristianno <pablochristianno@gmail.com>
2024-12-06 16:57:20 -0300

    fix: Resolve bugs no functor pergunta.

--- app/inference_engine.pl
+++ app/inference_engine.pl
@@ -1,20 +1,37 @@
+:- include('knowledge_base.pl').
+
 % Conta Pokemons para cada característica
 conta_caracteristica(Caracteristica, Valor, Contagem) :-
     findall(Pokemon, call(Caracteristica, Pokemon, Valor), Lista),
     length(Lista, Contagem).
 
+caracteristicas([tipo, altura, peso, formato, habitat, cor]).
+% Escolhe melhor pergunta a ser feita
 % Escolhe melhor pergunta a ser feita
 perguntar :-
+    caracteristicas(Caracteristicas),  % Obtém a lista de características
     findall(
         (Caracteristica, Valor, Contagem),
-        (current_predicate(Caracteristica/2), call(Caracteristica, _, Valor), conta_caracteristica(Caracteristica, Valor, Contagem)),
-        Opcoes
+        (
+            member(Caracteristica, Caracteristicas), 
+            call(Caracteristica, _, Valor),          % Obtém valores para a característica
+            conta_caracteristica(Caracteristica, Valor, Contagem)
+        ),
+        OpcoesNaoUnicas
     ),
-    selecionar_melhor_pergunta(Opcoes, MelhorPergunta),
-    format('A entidade possui ~w igual a ~w? (s/n) ', MelhorPergunta),
-    read(Resposta),
-    processar_resposta(Resposta, MelhorPergunta). % Atualiza a base de características e pokemons. (Implementar na filtragem)
+    sort(OpcoesNaoUnicas, Opcoes), % Remove duplicatas
+    selecionar_melhor_pergunta(Opcoes, (Carac, Valor)),
+    format('A entidade possui ~w igual a ~w? (s/n): ', [Carac, Valor]),
+    read(Resposta). 
+    % processar_resposta(Resposta, MelhorPergunta). % Atualiza a base de características e pokemons. (Implementar na filtragem)
 
 % Seleciona pergunta capaz de eleiminar mais Pokemons da base
 selecionar_melhor_pergunta(Opcoes, (Caracteristica, Valor)) :-
-    sort(3, @>=, Opcoes, [(Caracteristica, Valor, _)|_]).
+    maplist(extraia_terceiro, Opcoes, Chaves),
+    maplist(inverter_chave, Chaves, ChavesInvertidas), 
+    pairs_keys_values(Pares, ChavesInvertidas, Opcoes),
+    keysort(Pares, ParesOrdenados),
+    pairs_values(ParesOrdenados, [(Caracteristica, Valor, _)|_]).
+inverter_chave(Chave, ChaveInvertida) :-
+    ChaveInvertida is -Chave.
+extraia_terceiro((_, _, Terceiro), Terceiro).
